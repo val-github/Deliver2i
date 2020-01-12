34a702e6101e9eb8df642114b2b0d0a104279a3e
val <z305blet@gmail.com>
2020-01-12 20:24:57 +0100

    Revert "début V1"
    This reverts commit 42ca55e4b3c4b005645496428348ee5ade9d7495.

--- src/projet/PROJET.java
+++ src/projet/PROJET.java
@@ -5,17 +5,11 @@
  */
 package projet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 import modele.Instance;
-import modele.Tournee;
 
 /**
  *
@@ -25,39 +19,22 @@ public class PROJET {
     
     private Instance In;
     
-    //fonction de connection à l'instance
-    private Connection connection;
-    private void connect() throws ClassNotFoundException, SQLException {
-        String driverClass = "org.apache.derby.jdbc.ClientDriver";
-        String urlDatabase = "jdbc:derby://localhost:1527/Projet";
-        String user = "val";
-        String pwd = "100898";
-        Class.forName(driverClass);
-        connection = DriverManager.getConnection(urlDatabase, user, pwd);
-    }
-    //fonction de récupération des tournées stockées
-    private List<Tournee> ensTournee () throws SQLException {
-        List<Tournee> tournees = new ArrayList<>();
-        String requete = "SELECT * from instance";
-        try (Statement stmt = connection.createStatement(); ResultSet res = stmt.executeQuery(requete)) {
-            while (res.next()) {
-                Tournee t = new Tournee(res.getInt("Id_Tournee"), res.getInt("Horaire_debut"), res.getInt("Horaire_fin"));
-                tournees.add(t);
-            }
+    //private void AfficherInstance() throws SQLException{
+        //try 
+        {
+            //List<Instance> instance = this.In.ensInstance();
+            //DefaultListModel dlm = new DefaultListModel();
+            //instance.forEach(dlm::addElement);
         }
-        return tournees;
     }
-    
-    //création d'une liste avec les tournees
-    List<Tournee> tournees=ensTournee()
     /**
      * @param args the command line arguments
      */
-    //public static void main(String[] args) 
-    //{
+    public static void main(String[] args) 
+    {
         
         
         // TODO code application logic here
-    //}
+    }
     
-
+}
